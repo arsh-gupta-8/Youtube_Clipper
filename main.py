908ea9af9b3703fe1ccd38ee114aa30344ac93c6
arsh-gupta-8 <arshmailing@gmail.com>
2026-09-02 22:10:20 +0700

    Filtered comments to ones containing time stamps

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from youtube_comment_downloader import YoutubeCommentDownloader, SORT_BY_POPULAR
+from time import sleep
 
 downloader = YoutubeCommentDownloader()
 
-videoURL = "https://www.youtube.com/watch?v=8dTpNajxaH0" 
+videoURL = "https://www.youtube.com/watch?v=99lR1LpbBqA" 
 
 comments = downloader.get_comments_from_url(videoURL, sort_by=SORT_BY_POPULAR)
+timeStampComments = []
 
 for index, comment in enumerate(comments):
-    if index >= 10: 
-        break
-    print(f"[{comment['author']}]: {comment['text']}\n")
+  print(index)
+  if index == 500:
+    break 
+  if ":" in comment['text'] and (comment['text'][0].isdigit() or comment['text'][-1].isdigit()):
+    timeStampComments.append(comment["text"])
+
+print(timeStampComments)
