8252ed5b154e8fd7acd2c78765d81bb6ba05358a
arsh-gupta-8 <arshmailing@gmail.com>
2026-09-03 13:08:19 +0700

    obtained time stamps from each comment and converted to seconds format

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from youtube_comment_downloader import YoutubeCommentDownloader, SORT_BY_POPULAR
 from time import sleep
+import re
 
 downloader = YoutubeCommentDownloader()
 
 videoURL = "https://www.youtube.com/watch?v=99lR1LpbBqA" 
 
 comments = downloader.get_comments_from_url(videoURL, sort_by=SORT_BY_POPULAR)
-timeStampComments = []
+times = []
+
+timeStampPattern = r"(?:(\d+):)?(\d+):(\d\d)"
+
+
+def hms_to_seconds(hms):
+  hms = hms.split(":")
+  hms.reverse()
+  seconds = 0
+  for i in range(len(hms)):
+    seconds += int(hms[i]) * (60**i)
+  return seconds
+
 
 for index, comment in enumerate(comments):
-  print(index)
   if index == 500:
     break 
-  if ":" in comment['text'] and (comment['text'][0].isdigit() or comment['text'][-1].isdigit()):
-    timeStampComments.append(comment["text"])
+  timeStamp = re.search(timeStampPattern, comment['text'])
+  if timeStamp:
+    print(timeStamp.group())
+    timeStamp = timeStamp.group()
+    times.append(hms_to_seconds(hms=timeStamp))
 
-print(timeStampComments)
+# print(times)
